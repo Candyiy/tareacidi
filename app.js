d6072728ecaaff29e7398db9e76093d17672e306
candy <jbastaluego@gmail.com>
2025-12-15 04:08:28 -0400

    inicio

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mysql = require("mysql2");
+
+const app = express();
+const port = 3000;
+
+const db = mysql.createPool({
+    host: "mysql", 
+    user: "usuario",
+    password: "contrasena",
+    database: "upds2025",
+});
+
+app.get('/', (req, res) => {
+    db.query('SELECT * FROM pruebados', (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error en la base de datos');
+        }
+
+        let html = "<h2>Datos de la tabla prueba</h2>";
+        xdxd="";
+        results.forEach(row => {
+            xdxd += `Nombre: ${row.nombre} - Apellido: ${row.apellido}<br>`;
+        });
+
+        res.send(xdxd);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor Node escuchando en puerto ${port}`);
+});
